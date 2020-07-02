c356478a7034e07a361a0dc1eea1865e16221503
Boyd_Thurston <boydthurston@gmail.com>
2020-07-02 13:11:51 +1200

    define Users table

--- server/db/migrations/20200609151917_users.js
+++ server/db/migrations/20200609151917_users.js
@@ -4,6 +4,9 @@ exports.up = (knex, Promise) => {
    table.increments('id').primary()
    table.string('username').unique()
    table.string('hash')
+   table.string('email')
+   table.boolean('is_child').defaultTo(false)
+   table.integer('parent_id')
  }) 
 }
 
