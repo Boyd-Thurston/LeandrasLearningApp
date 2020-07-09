66b82fa77e078cdf6d8e81f174b3c8c2157b2612
Boyd_Thurston <boydthurston@gmail.com>
2020-07-09 13:55:48 +1200

    add children reducer

---
+++ client/reducers/children.js
@@ -0,0 +1,16 @@
+// local imports
+import { } from "../actions"
+
+// define initial state
+const initialState = []
+
+// define reducer
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default: 
+      return state
+  }
+}
+
+// export reducer
+export default reducer
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,10 +3,12 @@ import { combineReducers } from 'redux'
 
 // local imports
 import user from './user'
+import children from './children'
 
 // combine reducers to single reducer
 const reducer = combineReducers({
-  user
+  user,
+  children
 })
 
 // export single reducer
