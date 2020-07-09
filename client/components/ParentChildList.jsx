2984da3683204278abb2fb7698ac9a0fdc3232af
Boyd_Thurston <boydthurston@gmail.com>
2020-07-09 13:53:32 +1200

    add Children List component

---
+++ client/components/ParentChildList.jsx
@@ -0,0 +1,34 @@
+// import external modules
+import React from 'react'
+import { connect } from 'react-redux'
+
+// local imports
+
+// define class component
+class ParentChildList extends React.Component {
+  componentDidMount(){
+    // call thunk to fetch children added by this parent account
+  }
+
+  render(){
+    return(
+      <>
+        {this.props.children.length >= 1? 
+          // map list of children 
+          <p>I am the map of listed children</p>:
+          <p>you have no children registered</p>
+        }
+      </>
+    )
+  }
+}
+
+// map global state to the local props
+function mapStateToProps(globalState) {
+  return {
+    children: globalState.children
+  }
+}
+
+// export class component
+export default connect(mapStateToProps)(ParentChildList)
