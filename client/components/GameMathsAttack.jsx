9fae6375528fba3f4458092b115c2b140663f46d
Boyd_Thurston <boydthurston@gmail.com>
2020-07-22 20:56:13 +1200

    add GameMathsAttck component

---
+++ client/components/GameMathsAttack.jsx
@@ -0,0 +1,21 @@
+// import external modules
+import React from 'react'
+import { connect } from 'react-redux'
+
+// local imports
+
+// define class component
+class GameMathsAttack extends React.Component {
+
+  // render the component
+  render(){
+    return(
+      <>
+      </>
+    )
+  }
+}
+
+
+// export component
+export default connect()(GameMathsAttack)
