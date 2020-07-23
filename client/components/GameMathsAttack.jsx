352c60aef5ca89c97050031caa09b9354492377f
Boyd_Thurston <boydthurston@gmail.com>
2020-07-24 10:12:11 +1200

    add functionality to maths game component
    added inital state and compenent did mount generation function calls to
    repopuplate state once loaded

--- client/components/GameMathsAttack.jsx
+++ client/components/GameMathsAttack.jsx
@@ -3,9 +3,30 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 // local imports
+import { getRandomSelection, getRandomNumber } from '../utils/lib'
 
 // define class component
 class GameMathsAttack extends React.Component {
+  // set initial state
+  state = {
+    firstNumber = '',
+    secondNumber = '',
+    thirdNumber = '',
+    skillLevel = 1,
+    operator = ''
+  }
+
+  componentDidMount(){
+    this.setState({
+      firstNumber = getRandomNumber(1, 10),
+      secondNumber = getRandomNumber(1, 10),
+      thirdNumber = getRandomNumber(1, 10),
+      operator = getRandomSelection(['+', '-', 'x', '÷'])
+    })
+    
+  }
+
+  
 
   // render the component
   render(){
