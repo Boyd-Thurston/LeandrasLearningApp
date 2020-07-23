023635348e20c90f52ad312ae7d0de169ac90ce1
Boyd_Thurston <boydthurston@gmail.com>
2020-07-23 12:37:21 +1200

    refactor utils

--- client/utils/lib.js
+++ client/utils/lib.js
@@ -1,17 +1,24 @@
-// UTIITIES LIBARY FOR FRONTEND SUPPORTING FUNCTIONS
+// =========================================================
+//     UTIITIES LIBARY FOR FRONTEND SUPPORTING FUNCTIONS
+// =========================================================
 
 
+// ===============================
 // _____ AUTH/JWT FUNCTIONS ______
+// ===============================
+
 
 // set JWT token in local storage
 export function setToken (token) {
   localStorage.setItem('token', token)
 }
 
+
 // captures JWT details from local store
 export function getToken () {
   return localStorage.getItem('token')
 }
+
 
 // checks to see if user is authenticated or not
 export function isAuthenticated () {
@@ -23,7 +30,12 @@ export function isAuthenticated () {
   }
 }
 
+
+
+// ====================================
 // _______ RANDOMISER FUNCTIONS _______
+// ====================================
+
 
 // random number generater
 export function getRandomNumber (min, max ) {
@@ -34,6 +46,7 @@ export function getRandomNumber (min, max ) {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
+
 // random selection from given array
 export function getRandomSelection (array) {
   // get random value to use for index
@@ -42,12 +55,13 @@ export function getRandomSelection (array) {
   return array[randomInex]
 }
 
+
 // randomly shuffles the order of a given array
 export function getShuffledArray (array) {
   // set variables
   shuffledArray = []
   unshuffledArray = [...array]
-  
+
   // loop throuhg passed array
   while(shuffledArray.length() != array.length()){
     // get random value to use for index
