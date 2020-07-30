1a1b840db6f7723714a549665ce0dab80c4135da
Boyd_Thurston <boydthurston@gmail.com>
2020-07-30 13:32:40 +1200

    add game reducer

---
+++ client/reducers/game.js
@@ -0,0 +1,16 @@
+// local imports
+import { PARENT_SAVE_CHILDREN_LIST } from "../actions"
+
+// define initial state
+const initialState = []
+
+// define reducer
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default: 
+      return state
+  }
+}
+
+// export reducer
+export default reducer
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -4,11 +4,13 @@ import { combineReducers } from 'redux'
 // local imports
 import user from './user'
 import children from './children'
+import game from './game'
 
 // combine reducers to single reducer
 const reducer = combineReducers({
   user,
-  children
+  children,
+  game
 })
 
 // export single reducer
