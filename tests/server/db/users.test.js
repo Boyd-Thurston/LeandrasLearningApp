5f15abd7c39d1b65eac1386468b92d78b3c610ff
Boyd_Thurston <boydthurston@gmail.com>
2020-08-11 15:57:35 +1200

    pseudo code test calls for db user table test

--- tests/server/db/users.test.js
+++ tests/server/db/users.test.js
@@ -18,3 +18,17 @@ beforeAll(() => {
 test('test suite is fuctional for: "server/db/users.js"', () => {
   expect(1).toBeGreaterThan(0)
 })
+
+// test: createParentUser({username, password, email }, db = connection) 
+// extected: returns an id as a number
+
+// test: createChildUser({username, password, parent_id }, db = connection) extected to return an id
+
+// test: verifyUser(username, password , db = connection)
+// extected: returns an id as a number
+
+// test: getUserById(id, db = connection) 
+// expected: returns an object with props username, isChild, parentId
+
+// test: getChildrenByParentId(id, db = connection) 
+// expected: retuns an array of objects with props id, username
