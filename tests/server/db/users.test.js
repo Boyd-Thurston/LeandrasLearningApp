ef22bef1976721e3206609cdd37b7b3fe08e94fe
Boyd_Thurston <boydthurston@gmail.com>
2020-08-11 14:31:23 +1200

    set up test suite for users db functions

---
+++ tests/server/db/users.test.js
@@ -0,0 +1,20 @@
+// set up enviroment and connection
+const knex = require('knex')
+const config = require('../../../knexfile')
+const env = 'test'
+const database = knex(config[env])
+
+// local imports
+const db_user_functions = require('../../../server/db/users')
+
+// build and seed test database
+beforeAll(() => {
+  return database.migrate.latest()
+  // .then(() => {
+  //   return database.seed.run()
+  // })
+})
+
+test('test suite is fuctional for: "server/db/users.js"', () => {
+  expect(1).toBeGreaterThan(0)
+})
