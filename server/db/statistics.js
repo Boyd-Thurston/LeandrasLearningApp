d9422050c3a848918c0009b4f9446667104a839f
Boyd_Thurston <boydthurston@gmail.com>
2020-08-20 13:43:50 +1200

    set up statistics functions base connection

---
+++ server/db/statistics.js
@@ -0,0 +1,10 @@
+// STATISTICS TABLE DATABASE FUNCTIONS
+// 
+// this set of functions contains all the db queries the relate 
+// specificaly to the statistics table in the db.
+
+// local imports
+const connection = require('./connection')
+
+// export functions
+module.exports = {}
