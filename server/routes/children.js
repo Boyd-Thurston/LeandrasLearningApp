73222938c22d0059cb9144cebafe7f7c99780dfa
Boyd_Thurston <boydthurston@gmail.com>
2020-09-21 11:44:25 +1200

    add rout to retreaive a facts list for the db

--- server/routes/children.js
+++ server/routes/children.js
@@ -1,12 +1,24 @@
 // import external modules
 const express = require('express')
 
+// local imports
+const { getFactsList } = require('../db/facts')
+
 // define router
 const router = express.Router()
 
 // define routes
+router.get('/facts', getFacts)
 
 // supporting functions to routs
+function getFacts (req, res) {
+  getFactsList(req.user.id)
+    .then(factList => res.json(factList))
+    .catch(() => res.status(500).json({
+      ok: false,
+      message: 'An error occurred while retrieving data from the database'
+    }))
+}
 
 // export router
 module.exports = router
