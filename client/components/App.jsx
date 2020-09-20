fc6a8e2c33e7bf97b2afc07a260df1da5ebca63f
Boyd_Thurston <boydthurston@gmail.com>
2020-09-21 10:31:15 +1200

    FIX: Logout redirect broken link
    refactored Nav functional component to class component
    and moved log off functionality from App to Nav
    allowing history.push to function correctly

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 // local imports
-import { USER_LOG_OFF, saveUser } from '../actions'
+import { saveUser } from '../actions'
 import { isAuthenticated } from '../utils/lib'
 import { getUserDetails } from '../apis'
 import Nav from './Nav'
@@ -24,37 +24,13 @@ export class App extends React.Component {
     isAuthenticated() && getUserDetails()
     .then(userDetails => {this.props.dispatch(saveUser(userDetails))})
   }
-  
-  // event handler for user log off
-  handleLogOff = () => {
-    this.props.dispatch({type: USER_LOG_OFF})
-    this.props.history.push('/')
-  }
+
 
   // render the App component
   render(){
     return(
       <Router>
-        <Route path="/">
-          <Nav>
-            {isAuthenticated()? 
-            <>
-              {/* log out functionality call/username display */}
-              <p>Hello {this.props.user.username}</p>
-              <button className="button" onClick={this.handleLogOff}>Log Off</button>
-            </>
-            : 
-            <>
-              <p>you are not signed in</p>
-              <Link to="/auth/signin">
-                <button className="button"> Sign In </button>
-              </Link>
-              <Link to="/auth/register">
-                <button className="button"> Register </button>
-              </Link>
-            </>}
-          </Nav>
-        </Route>
+        <Route path="/" component={Nav}/>
         <Route exact path="/" component={Home}/>
         <Route path="/auth/signin" component={AuthSignIn}/>
         <Route exact path="/auth/register" component={AuthRegister}/>
--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,20 +1,59 @@
+// import external modules
 import React from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
+// local imports
+import { isAuthenticated } from '../utils/lib'
+import { USER_LOG_OFF } from '../actions'
 
-function Nav ({children}) {
-  return(
-      <nav id="nav-bar">
-        <div id="nav-bar-icon">
-          <Link to="/">
-            <img src="/android-chrome-192x192.png" alt="logo"/>
-          </Link>
-        </div>
-        <div id="nav-bar-children-container">
-          {children}
-        </div>
-      </nav>
-  )
+// define class component
+export class Nav extends React.Component {
+  
+  // event handler for user log off
+  handleLogOff = () => {
+    this.props.dispatch({type: USER_LOG_OFF})
+    this.props.history.push('/')
+  }
+  
+  // render class component
+  render(){
+    return(
+        <nav id="nav-bar">
+          <div id="nav-bar-icon">
+            <Link to="/">
+              <img src="/android-chrome-192x192.png" alt="logo"/>
+            </Link>
+          </div>
+          <div id="nav-bar-children-container">
+          {isAuthenticated()? 
+              <>
+                {/* log out functionality call/username display */}
+                <p>Hello {this.props.user.username}</p>
+                <button className="button" onClick={this.handleLogOff}>Log Off</button>
+              </>
+              : 
+              <>
+                <p>you are not signed in</p>
+                <Link to="/auth/signin">
+                  <button className="button"> Sign In </button>
+                </Link>
+                <Link to="/auth/register">
+                  <button className="button"> Register </button>
+                </Link>
+              </>}
+          </div>
+        </nav>
+    )
+  }
 }
 
-export default Nav
+// map global state to the local props
+function mapStateToProps(globalState) {
+  return {
+    user: globalState.user
+  }
+}
+
+// export component
+export default connect(mapStateToProps)(Nav)
