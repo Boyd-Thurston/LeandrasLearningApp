eff8938d63f6fd34e9b99a08397e41fd47395ff0
Boyd_Thurston <boydthurston@gmail.com>
2020-09-29 12:24:57 +1300

    add seed data for excerpts
    added gathered data to seed and reworked migration to fit with seed data

--- server/db/migrations/20200921114603_excerpts.js
+++ server/db/migrations/20200921114603_excerpts.js
@@ -2,9 +2,11 @@
 exports.up = knex => {
   return knex.schema.createTable('excerpts', table => {
     table.increments('id').primary()
+    table.string('title')
+    table.string('author')
     table.text('excerpt')
     table.text('possible_answers')
-    table.text('question')
+    table.string('question')
     table.string('answer')
   }) 
  }
